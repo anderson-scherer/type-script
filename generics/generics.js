86cc4d216158a239b95bb70a398ceb227cfced98
Anderson Scherer <anderson.scherer.costa@gmail.comm>
2020-02-07 17:36:52 -0300

    Genérics

--- generics/generics.js
+++ generics/generics.js
@@ -16,8 +16,8 @@ console.log(echoMelhorado({ nome: 'João' }).nome); // João
 // Generics Disponíveis na API
 const avaliacoes = [10, 9.3, 7.7];
 avaliacoes.push(8.4);
-// avaliacoes.push('5.5'); // rro de compilador
-avaliacoes.push(5.5); // rro de compilador
+// avaliacoes.push('5.5'); // erro de compilador
+avaliacoes.push(5.5);
 console.log(avaliacoes);
 // Array
 function imprimir(args) {
@@ -34,4 +34,33 @@ imprimir([
     { nome: 'João' },
     { nome: 'João' },
 ]);
+const chamarEco = echoMelhorado;
+// const chamarEco: <T>(data: T) => T = echoMelhorado;
+console.log(chamarEco('Alguma Coisa'));
+// Classe
+// class OperacaoBinaria {
+//     constructor(
+//         public operando1: any,
+//         public operacao2: any
+//     ) { }
+//     executar() {
+//         return this.operando1 + this.operacao2
+//     }
+// }
+// console.log(new OperacaoBinaria('Bom', 'Dia').executar());
+// console.log(new OperacaoBinaria(3, 7).executar());
+// console.log(new OperacaoBinaria(3, 'Opa').executar());
+// console.log(new OperacaoBinaria({}, {}).executar());
+class OperacaoBinaria {
+    constructor(operando1, operacao2) {
+        this.operando1 = operando1;
+        this.operacao2 = operacao2;
+    }
+}
+class SomaBinaria extends OperacaoBinaria {
+    executar() {
+        return this.operando1 + this.operacao2;
+    }
+}
+console.log(new SomaBinaria(3, 4).executar());
 //# sourceMappingURL=generics.js.map
--- generics/generics.ts
+++ generics/generics.ts
@@ -19,27 +19,68 @@ console.log(echoMelhorado({ nome: 'João' }).nome); // João
 // Generics Disponíveis na API
 const avaliacoes: Array<number> = [10, 9.3, 7.7];
 avaliacoes.push(8.4);
-// avaliacoes.push('5.5'); // rro de compilador
-avaliacoes.push(5.5); // rro de compilador
+// avaliacoes.push('5.5'); // erro de compilador
+avaliacoes.push(5.5);
 console.log(avaliacoes);
 
 // Array
-
 function imprimir<T>(args: T[]) {
     args.forEach(elemento => console.log(elemento));
 }
 
-imprimir([1,2,4])
-imprimir<number>([1,2,4])
-imprimir<string>(['1','2','4'])
-imprimir< {nome: string} >([
-    { nome : 'João'},
-    { nome : 'João'},
+imprimir([1, 2, 4])
+imprimir<number>([1, 2, 4])
+imprimir<string>(['1', '2', '4'])
+imprimir<{ nome: string }>([
+    { nome: 'João' },
+    { nome: 'João' },
 ])
 
-type Aluno = {nome: string};
+type Aluno = { nome: string };
 
 imprimir<Aluno>([
-    { nome : 'João'},
-    { nome : 'João'},
+    { nome: 'João' },
+    { nome: 'João' },
 ])
+
+// Tipo generics
+type Echo = <T>(data: T) => T;
+const chamarEco: Echo = echoMelhorado;
+
+// const chamarEco: <T>(data: T) => T = echoMelhorado;
+
+console.log(chamarEco<string>('Alguma Coisa'));
+
+// Classe
+// class OperacaoBinaria {
+//     constructor(
+//         public operando1: any,
+//         public operacao2: any
+//     ) { }
+
+//     executar() {
+//         return this.operando1 + this.operacao2
+//     }
+// }
+
+// console.log(new OperacaoBinaria('Bom', 'Dia').executar());
+// console.log(new OperacaoBinaria(3, 7).executar());
+// console.log(new OperacaoBinaria(3, 'Opa').executar());
+// console.log(new OperacaoBinaria({}, {}).executar());
+
+abstract class OperacaoBinaria<T, R> {
+    constructor(
+        public operando1: T,
+        public operacao2: T
+    ) { }
+
+    abstract executar(): R;
+}
+
+class SomaBinaria extends OperacaoBinaria<number, number> {
+    executar(): number {
+        return this.operando1 + this.operacao2;
+    }
+}
+
+console.log(new SomaBinaria(3, 4).executar());
